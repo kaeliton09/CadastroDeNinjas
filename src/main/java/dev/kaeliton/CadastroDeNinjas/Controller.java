f18c671a32193463fcb6c88cbff0ad37bf78b347
K4eliton <cardosokaeliton@gmail.com>
2026-07-07 09:55:01 -0300

    Criado a Controller

---
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Controller.java
@@ -0,0 +1,19 @@
+package dev.kaeliton.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "essa eh minha primeira mensagem";
+    }
+
+
+
+
+}
