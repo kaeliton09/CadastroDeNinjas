cd18896151d4bc204d81d5efa072dd79bccd18de
K4eliton <cardosokaeliton@gmail.com>
2026-07-08 11:37:39 -0300

    fix: Coloca a coluna de email como unique

--- src/main/java/dev/kaeliton/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -19,6 +19,8 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @Column(unique = true) // coluna unica
     private String email;
     private int idade;
 
