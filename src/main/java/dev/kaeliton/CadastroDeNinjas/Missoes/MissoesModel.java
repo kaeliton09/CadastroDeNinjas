7faabcc8f14018533b8e72496c7207c5aeb20896
K4eliton <cardosokaeliton@gmail.com>
2026-07-08 11:31:31 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.kaeliton.CadastroDeNinjas.Missoes;
 
 import dev.kaeliton.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -19,44 +25,4 @@ public class MissoesModel {
     // @OneToMany 1 missao tem 1 ou n ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel(String nome, String dificuldade, String descricao) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.descricao = descricao;
-    }
-
-    public MissoesModel(String nome, String dificuldade, String descricao, List<NinjaModel> ninjas) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.descricao = descricao;
-        this.ninjas = ninjas;
-    }
-
-    public MissoesModel() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
