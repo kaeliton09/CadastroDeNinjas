91257f5f8dd03f1e9b7b41a6f2bf1a0961039086
K4eliton <cardosokaeliton@gmail.com>
2026-07-27 15:48:59 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,32 @@
 package dev.kaeliton.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes") // -> localhost:8080/"misseos"
 public class MissoesController {
+
+    // Adicionar Missao (CREATE)
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "criando missao";
+    }
+
+    // Mostrar Missoes (READ)
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "listando missoes";
+    }
+
+    // Alterar Missao por ID (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarMissaoPorId(){
+        return "alterar missao por id";
+    }
+
+    // Deletar Missao por ID (DELETE)
+    @DeleteMapping("/deletarId")
+    public String deletarMissaoPorId(){
+        return "deletar missao por id";
+    }
 }
