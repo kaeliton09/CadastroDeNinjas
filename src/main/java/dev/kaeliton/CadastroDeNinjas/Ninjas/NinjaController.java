6acdca340b6c59a4800bfd790739c679c16b86e4
K4eliton <cardosokaeliton@gmail.com>
2026-07-31 11:03:38 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta a dependencia do service e do repository

--- src/main/java/dev/kaeliton/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,9 +2,17 @@ package dev.kaeliton.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
@@ -20,8 +28,8 @@ public class NinjaController {
 
     // Mostrar todos os Ninjas (Read)
     @GetMapping("/listar")
-    public String mostrarNinjas(){
-        return "sem ninjas no momento";
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
     }
 
     // Mostrar ninja por id (Read)
--- src/main/java/dev/kaeliton/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,22 @@
 package dev.kaeliton.CadastroDeNinjas.Ninjas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+
+    private NinjasRepository ninjasRepository;
+
+    public NinjaService(NinjasRepository ninjasRepository) {
+        this.ninjasRepository = ninjasRepository;
+    }
+
+    // listar todos os ninjas
+    public List<NinjaModel> listarNinjas(){
+        return ninjasRepository.findAll();
+    }
 }
