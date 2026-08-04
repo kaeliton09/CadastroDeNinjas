c28789052bb144d0236f84c7adbdf219ccbcb641
K4eliton <cardosokaeliton@gmail.com>
2026-08-04 10:18:19 -0300

    fear: Adiciona as Rotas de Criar, ALterar, Listar, Listar Por Id e Delecao das Missoes

--- src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,31 +2,45 @@ package dev.kaeliton.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/missoes") // -> localhost:8080/"misseos"
 public class MissoesController {
 
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     // Adicionar Missao (CREATE)
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "criando missao";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+        return missoesService.criarMissao(missao);
     }
 
     // Mostrar Missoes (READ)
     @GetMapping("/listar")
-    public String listarMissoes(){
-        return "listando missoes";
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
+    }
+
+    // Mostrar Missoes por Id(READ)
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissoesPorId(@PathVariable Long id){
+        return missoesService.listarMissoesPorId(id);
     }
 
     // Alterar Missao por ID (UPDATE)
-    @PutMapping("/alterarID")
-    public String alterarMissaoPorId(){
-        return "alterar missao por id";
+    @PutMapping("/alterar/{id}")
+    public MissoesModel alterarMissaoPorId(@PathVariable Long id, @RequestBody MissoesModel missao){
+        return missoesService.alterarMissaoPorId(id, missao);
     }
 
     // Deletar Missao por ID (DELETE)
-    @DeleteMapping("/deletarId")
-    public String deletarMissaoPorId(){
-        return "deletar missao por id";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissaoPorId(@PathVariable Long id){
+        missoesService.deletarMissaoPorId(id);
     }
 }
--- src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/kaeliton/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,43 @@
 package dev.kaeliton.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // listar todas as missoes
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    // listar missao por id
+    public MissoesModel listarMissoesPorId(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
+    }
+
+    // criar nova missao
+    public MissoesModel criarMissao(MissoesModel missao){
+        return missoesRepository.save(missao);
+    }
+
+    // deletar missao por id
+    public void deletarMissaoPorId(Long id){
+        missoesRepository.deleteById(id);
+    }
+
+    // alterar missao por id
+    public MissoesModel alterarMissaoPorId(Long id, MissoesModel missao){
+        missao.setId(id);
+        return missoesRepository.save(missao);
+    }
 }
